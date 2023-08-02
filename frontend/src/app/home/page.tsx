5e773427b5f75b67827be68839ee253f922cda5e
Deena KC <dinakc14@gmail.com>
2023-08-02 19:04:47 -0400

    create new branch

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Homeblogs() {
   return (
-    <div className=" h-96 bg-yellow-300 flex flex-col align-center justify-around">
+    <div className=" h-96 bg-yellow-300 flex flex-col align-center justify-around p-5">
       <h1 className="text-7xl "> Happy Blogging</h1>
       <h3 className=" text-2xl">
         A safe create space for our beloved bloggers for creating unique blogs
