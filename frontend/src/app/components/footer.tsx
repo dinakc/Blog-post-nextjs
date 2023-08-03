165765e2e6c195fbc64b50472e560a197e202c39
Deena KC <dinakc14@gmail.com>
2023-08-03 00:28:15 -0400

    fetch popular blog data from axios

--- frontend/src/app/components/footer.tsx
+++ frontend/src/app/components/footer.tsx
@@ -1,8 +1,10 @@
 function Footer() {
   return (
-    <h1 className="absolute right-0 left-0 bottom-0 h-10 bg-slate-900 text-white text-center p-2">
-      Copywrite &copy; 2023
-    </h1>
+    <div className="relative">
+      <h1 className="absolute right-0 left-0 bottom-0 h-10 bg-slate-900 text-white text-center p-2">
+        Copywrite &copy; 2023
+      </h1>
+    </div>
   );
 }
 
--- frontend/src/app/components/homeblogs.tsx
+++
@@ -1,9 +0,0 @@
-function Homeblogs() {
-  return (
-    <div>
-      <h1> Our Popular Blogs</h1>
-    </div>
-  );
-}
-
-export default Homeblogs;
