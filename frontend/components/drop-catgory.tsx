42467011491c0a3c2043d08609cc68b3f1d341c2
Ashma Upreti <ashma.upreti99@gmail.com>
2023-08-06 23:40:25 -0400

    styled navbar and create protected route

---
+++ frontend/components/drop-catgory.tsx
@@ -0,0 +1,19 @@
+// const category_list = ["Login", "Logout", "SignUp"];
+
+// function DropMenu() {
+//   return (
+//     <>
+//       <div className="absolute top-full left-0 bg-red-700 p-2 mt-1 z-999">
+//         {category_list.map((category, index) => (
+//           <div key={`div_${index}`}>
+//             <a href="#" className="block text-white hover:bg-slate-900 p-2">
+//               {category}
+//             </a>
+//           </div>
+//         ))}
+//       </div>
+//     </>
+//   );
+// }
+
+// export default DropMenu;
--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,12 +1,30 @@
+"use client";
+import { Elsie } from "next/font/google";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { BsPerson } from "react-icons/bs";
+// import DropMenu from "./drop-catgory";
 
 function Navbar() {
+  const router = useRouter();
+  const token = localStorage.getItem("session-token");
+  function checkLogin() {
+    if (!token) {
+      router.push("/register");
+    } else {
+      router.push("/blog-form");
+    }
+  }
   return (
-    <div className="flex flex-row justify-around items-center bg-slate-900 text-white h-16">
-      <Link href="/home">Home</Link>
-      <Link href="/blogs">Blogs</Link>
-      <Link href="/login">About Us</Link>
-      <Link href="/register">Login</Link>
+    <div className="relative">
+      <div className="flex flex-row justify-around items-center bg-slate-900 text-white h-16">
+        <Link href="/home">Home</Link>
+        <Link href="/blogs">Blogs</Link>
+        <Link href="/about">About Us</Link>
+
+        <BsPerson onClick={checkLogin} />
+      </div>
     </div>
   );
 }
